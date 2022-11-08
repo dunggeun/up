4deab1738f99b4f2b98ef9ab761196d5bd1b4be9
leegeunhyeok <dev.ghlee@gmail.com>
2022-11-08 22:14:17 +0900

    fix(android): jni 자바 클래스 경로 수정
    fabric 활성화 후 빌드시, 앱이 즉시 중지되는 이슈가 있어 이를
    수정했습니다.
    
    - 패키지 구조가 변경되었으나, jni 네이티브 코드에서 경로가 대응되지 않아
      발생한 이슈입니다.
    
    refs #7

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/up/newarchitecture/components/MainComponentsRegistry;";
+      "Ldev/geundung/up/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
