5d3a71e07f1e6d4f037b5dbea98dbf3760ee2116
leegeunhyeok <dev.ghlee@gmail.com>
2022-11-09 02:55:24 +0900

    refactor: 스택 네비게이터 위치 변경
    screens > navigators 로 이동
    
    refs #10

---
+++ src/navigators/Navigator.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { RootStackNavigator } from './RootStack';
+
+export function Navigator(): JSX.Element {
+  return (
+    <>
+      <RootStackNavigator />
+      {/* other stacks */}
+    </>
+  );
+}
