64a3b991d04a23bed1311ccbdfad59674f3de38b
leegeunhyeok <dev.ghlee@gmail.com>
2022-11-11 02:56:13 +0900

    feat: dripsy 환경 구성
    - dripsy 라이브러리 추가
    - 기본 테마 정의 및 provider 구성 완료
    
    close #8

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import React, { Suspense } from 'react';
+import { DripsyProvider } from 'dripsy';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { SplashScreen } from './src/screens';
+import { themeLight } from './src/themes';
 import type { PropsWithChildren } from 'react';
 
 // eslint-disable-next-line import/no-named-as-default-member
@@ -10,9 +12,11 @@ const Navigator = React.lazy(() => import('./src/navigators'));
 function AppProviders<T = unknown>({ children }: PropsWithChildren<T>): JSX.Element {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        {children}
-      </NavigationContainer>
+      <DripsyProvider theme={themeLight}>
+        <NavigationContainer>
+          {children}
+        </NavigationContainer>
+      </DripsyProvider>
     </SafeAreaProvider>
   );
 }
