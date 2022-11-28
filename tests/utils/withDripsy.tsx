be6794fa0b65be2fb9934975e9c2e7f35a05855a
leegeunhyeok <dev.ghlee@gmail.com>
2022-11-28 23:14:28 +0900

    test: 테스팅 환경 개선
    dripsy 기반 컴포넌트를 정상적으로 렌더링 하기 위해서는
    DripsyProvider 로 wrapping 되어있어야 하기에 관련 유틸 함수를 구현하고
    테스트 코드에 적용함
    
    - withDripsy 유틸 함수 구현
    - 테스트 코드 대상 ESLint 구성 수정
    
    close #44

---
+++ tests/utils/withDripsy.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { DripsyProvider } from 'dripsy';
+import { themeLight } from 'src/themes';
+
+export const withDripsy = (component: React.ReactElement): JSX.Element => (
+  <DripsyProvider theme={themeLight}>
+    {component}
+  </DripsyProvider>
+);
