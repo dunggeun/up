0f7f534777502cd42e454d0a6007c21c140a2b3b
leegeunhyeok <dev.ghlee@gmail.com>
2022-12-02 02:12:39 +0900

    test: ProgressBar 컴포넌트 테스트 추가
    close #51

---
+++ src/designs/atoms/ProgressBar/__tests__/ProgressBar.spec.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render as testRender, screen, cleanup } from '@testing-library/react-native';
+import { faker } from '@faker-js/faker';
+import { withDripsy } from 'tests/utils';
+import { getColors } from 'src/themes/utils';
+import { ProgressBar } from '../ProgressBar';
+
+import type { ProgressBarProps } from '../ProgressBar';
+import type { colors } from 'src/themes/colors';
+
+const render = (props: ProgressBarProps): ReturnType<typeof testRender> =>
+  testRender(withDripsy(<ProgressBar {...props} />));
+
+describe('atoms/ProgressBar', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  describe('값을 지정했을 때', () => {
+    const MIN = 0;
+    let value: number;
+    let max: number;
+    let color: keyof typeof colors;
+
+    beforeEach(() => {
+      max = faker.datatype.number({ min: 50, max: 100 });
+      value = faker.datatype.number({ min: MIN, max });
+      color = faker.helpers.arrayElement<keyof typeof colors>(getColors());
+    });
+  
+    it('a11y value 속성이 정상적으로 설정되어야 한다', () => {
+      render({ value, max, color });
+
+      expect(screen.getByA11yValue({ min: MIN, max, now: value })).not.toBeNull();
+    });
+  });
+});
