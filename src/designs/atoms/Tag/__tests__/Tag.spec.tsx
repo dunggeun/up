256d78348a9251d8fd8308c39664fa73d1eeab50
leegeunhyeok <dev.ghlee@gmail.com>
2022-12-02 00:52:27 +0900

    test: Tag 컴포넌트 테스트 추가
    close #50

---
+++ src/designs/atoms/Tag/__tests__/Tag.spec.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { render as testRender, screen, cleanup } from '@testing-library/react-native';
+import { faker } from '@faker-js/faker';
+import { withDripsy } from 'tests/utils';
+import { getColors } from 'src/themes/utils';
+import { Tag } from '../Tag';
+
+import type { colors } from 'src/themes/colors';
+import type { TagProps } from '../Tag';
+
+const render = (props: TagProps): ReturnType<typeof testRender> =>
+  testRender(withDripsy(<Tag {...props} />));
+
+describe('atoms/Tag', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  describe('태그 라벨을 지정했을 때', () => {
+    let label = '';
+    let color: keyof typeof colors;
+
+    beforeEach(() => {
+      label = faker.word.noun();
+      color = faker.helpers.arrayElement<keyof typeof colors>(getColors());
+    });
+  
+    it('onChange 이벤트 핸들러가 호출되어야 한다', () => {
+      render({ label, color });
+
+      expect(screen.getByText(label)).not.toBeNull();
+    });
+  });
+});
