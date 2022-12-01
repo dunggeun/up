41eea20bcb19dd2b992c4aaf561cb04bff4633cb
leegeunhyeok <dev.ghlee@gmail.com>
2022-12-02 00:49:27 +0900

    feat(atoms): Tag 컴포넌트 구현
    - atoms/Tag 컴포넌트 구현
    
    refs #50

---
+++ src/designs/atoms/Tag/Tag.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { styled, View, Text } from 'dripsy';
+import { isLight } from 'src/themes/utils';
+
+import type { ComponentPropsWithRef } from 'react';
+import type { colors } from 'src/themes/colors';
+
+type ViewProps = ComponentPropsWithRef<typeof View>;
+ 
+export interface TagProps extends ViewProps {
+  label: string;
+  color: keyof typeof colors;
+}
+
+const BORDER_WIDTH = 2;
+
+const Container = styled(View)(({ color }: Pick<TagProps, 'color'>) => ({
+  alignSelf: 'baseline',
+  paddingX: '$02',
+  paddingY: '$01',
+  borderRadius: '$full',
+  borderWidth: BORDER_WIDTH,
+  borderColor: 'text_primary',
+  backgroundColor: color,
+}));
+
+export function Tag ({ label, color, ...restProps }: TagProps): JSX.Element {
+  const labelVariant = isLight(color) ? 'primary' : 'white';
+
+  return (
+    <Container {...restProps} color={color} >
+      <Text variant={labelVariant}>{label}</Text>
+    </Container>
+  );
+}
