05e4917e1746b631175f7233aee75b99df7a656f
leegeunhyeok <dev.ghlee@gmail.com>
2022-12-02 02:01:07 +0900

    feat(atoms): ProgressBar 컴포넌트 구현
    refs #51

---
+++ src/designs/atoms/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import { styled, useDripsyTheme, View } from 'dripsy';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from 'react-native-reanimated';
+
+import type { ComponentPropsWithRef } from 'react';
+import type { colors } from 'src/themes/colors';
+
+type ViewProps = ComponentPropsWithRef<typeof View>;
+
+export interface ProgressBarProps extends Partial<ViewProps> {
+  value: number;
+  color: keyof typeof colors;
+  max: number;
+}
+
+const BORDER_WIDTH = 2;
+
+const Container = styled(View)({
+  position: 'relative',
+  height: 16,
+  borderRadius: '$full',
+  borderWidth: BORDER_WIDTH,
+  borderColor: 'text_primary',
+  backgroundColor: '$white',
+  overflow: 'hidden',
+});
+
+const styles = StyleSheet.create({
+  barDefault: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    height: '100%',
+  },
+});
+
+const calculatePercent = (value: number, max: number): number =>
+  Math.min(Math.max(0, value / max * 100), 100);
+
+export function ProgressBar ({
+  value,
+  max,
+  color,
+  ...restProps
+}: ProgressBarProps): JSX.Element {
+  const { theme } = useDripsyTheme();
+  const percent = useSharedValue(calculatePercent(value, max));
+  const barWidthStyle = useAnimatedStyle(() => ({ width: `${percent.value}%` }), []);
+  const barColorStyle = { backgroundColor: theme.colors[color] };
+
+  useEffect(() => {
+    percent.value = withSpring(calculatePercent(value, max));
+  }, [percent, value, max]);
+
+  return (
+    <Container {...restProps} accessibilityValue={{ min: 0, max, now: value }}>
+      <Animated.View style={[styles.barDefault, barColorStyle, barWidthStyle]} />
+    </Container>
+  );
+}
