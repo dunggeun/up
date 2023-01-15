b1633b5eed293396e0f01a11cb71302f521b3f14
leegeunhyeok <dev.ghlee@gmail.com>
2023-01-15 17:11:35 +0900

    refactor: 버튼 애니메이션 및 제스쳐 로직 훅으로 분리
    refs #69

--- src/designs/atoms/Button/Button.tsx
+++ src/designs/atoms/Button/Button.tsx
@@ -1,16 +1,10 @@
-import React, { useEffect, useMemo } from 'react';
-import { Platform } from 'react-native';
-import { GestureDetector, Gesture } from 'react-native-gesture-handler';
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  withDelay,
-  withTiming,
-  runOnJS,
-} from 'react-native-reanimated';
+import React from 'react';
+import { GestureDetector } from 'react-native-gesture-handler';
+import Animated from 'react-native-reanimated';
 import { styled, useSx, View, Text } from 'dripsy';
-import Haptic from 'react-native-haptic-feedback';
 import { isLight } from 'src/themes/utils';
+import { useAnimatedStyleWithGesture } from './hooks';
+import { BORDER_WIDTH, BUTTON_HEIGHT, PRESS_DEPTH } from './constants';
 
 import type { ViewStyle, ViewProps } from 'react-native';
 import type { colors } from 'src/themes/colors';
@@ -24,44 +18,34 @@ export interface ButtonProps extends AccessibilityProps {
   label: string;
   color: keyof typeof colors;
   active?: boolean;
-  haptic?: boolean;
+  disableHaptic?: boolean;
+  disableLongPress?: boolean;
   containerStyle?: ViewStyle;
   onPress?: () => void;
   onLongPress?: () => void;
 }
 
-const HEIGHT = 56;
-const BORDER_WIDTH = 2;
-const DEPTH = 5;
-const RELEASE_DURATION = 100;
-const LONG_PRESS_DURATION = 500;
-const LONG_PRESS_DELAY = 200;
-
 const Container = styled(View)({
   position: 'relative',
-  height: HEIGHT,
+  height: BUTTON_HEIGHT,
 });
 
 const Shadow = styled(View)({
   position: 'absolute',
   left: 0,
-  bottom: -DEPTH,
+  bottom: -PRESS_DEPTH,
   width: '100%',
   height: '100%',
   backgroundColor: '$text_primary',
   borderRadius: '$md',
 });
 
-function triggerHaptic (): void {
-  Haptic.trigger('impactMedium');
-}
-
 export function Button ({
   label,
   color,
   containerStyle,
-  active,
-  haptic = true,
+  disableHaptic = false,
+  disableLongPress = false,
   accessibilityHint,
   accessibilityLabel,
   accessibilityState,
@@ -69,10 +53,17 @@ export function Button ({
   onLongPress,
 }: ButtonProps): JSX.Element {
   const sx = useSx();
-  const capPosition = useSharedValue(0);
-  const dimPosition = useSharedValue(0);
-  const capAnimatedStyle = useAnimatedStyle(() => ({ top: capPosition.value }));
-  const dimAnimatedStyle = useAnimatedStyle(() => ({ top: capPosition.value, width: `${dimPosition.value}%` }));
+  const {
+    pressGesture,
+    longPressGesture,
+    capStyle: animatedCapStyle,
+    dimStyle: animatedDimStyle,
+  } = useAnimatedStyleWithGesture({
+    disableHaptic,
+    disableLongPress,
+    onPress,
+    onLongPress
+  });
   const isLightColor = isLight(color);
   const labelVariant = isLightColor ? 'primary' : 'white';
   const dimColor = isLightColor ? '$black' : '$white';
@@ -100,44 +91,6 @@ export function Button ({
     opacity: .2,
   });
 
-  const longPressGesture = useMemo(() => (
-    Gesture
-      .LongPress()
-      .minDuration(LONG_PRESS_DURATION)
-      .onBegin(() => {
-        dimPosition.value = withDelay(
-          LONG_PRESS_DELAY,
-          withTiming(100, {
-            duration: LONG_PRESS_DURATION - LONG_PRESS_DELAY
-          })
-        );
-      })
-      .onStart(() => onLongPress && runOnJS(onLongPress)())
-      .onEnd(() => {
-        capPosition.value = withTiming(0, { duration: RELEASE_DURATION });
-      })
-      .onFinalize(() => {
-        dimPosition.value = withTiming(0, { duration: RELEASE_DURATION });
-      })
-  ), [capPosition, dimPosition, onLongPress]);
-
-  const pressGesture = useMemo(() => (
-    Gesture.Tap()
-      .onTouchesDown(() => {
-        // Haptic feedback on only iOS
-        Platform.OS === 'ios' && haptic && runOnJS(triggerHaptic)();
-        capPosition.value = DEPTH;
-      })
-      .onTouchesUp(() => {
-        capPosition.value = withTiming(0, { duration: RELEASE_DURATION });
-      })
-      .onEnd(() => onPress && runOnJS(onPress)())
-  ), [capPosition, haptic, onPress]);
-
-  useEffect(() => {
-    capPosition.value = active ? DEPTH : 0;
-  }, [capPosition, active]);
-
   return (
     <GestureDetector gesture={longPressGesture}>
       <GestureDetector gesture={pressGesture}>
@@ -150,10 +103,10 @@ export function Button ({
           style={containerStyle}
         >
           <Shadow />
-          <Animated.View style={[capStyle, capAnimatedStyle]}>
+          <Animated.View style={[capStyle, animatedCapStyle]}>
             <Text variants={[labelVariant, 'h2']}>{label}</Text>
           </Animated.View>
-          <Animated.View style={[dimStyle, dimAnimatedStyle]} />
+          <Animated.View style={[dimStyle, animatedDimStyle]} />
         </Container>
       </GestureDetector>
     </GestureDetector>
---
+++ src/designs/atoms/Button/hooks/useAnimatedStyleWithGesture.ts
@@ -0,0 +1,90 @@
+import { useMemo } from 'react';
+import { Platform, type ViewStyle } from 'react-native';
+import {
+  Gesture,
+  type TapGesture,
+  type LongPressGesture
+} from 'react-native-gesture-handler';
+import {
+  useSharedValue,
+  useAnimatedStyle,
+  withDelay,
+  withTiming,
+  runOnJS
+} from 'react-native-reanimated';
+import Haptic from 'react-native-haptic-feedback';
+import {
+  PRESS_DEPTH,
+  LONG_PRESS_DELAY,
+  LONG_PRESS_DURATION,
+  RELEASE_DURATION
+} from '../constants';
+
+interface AnimatedStyleWithGestureConfig {
+  disableHaptic: boolean;
+  disableLongPress: boolean;
+  onPress?: () => void;
+  onLongPress?: () => void;
+}
+
+function triggerHaptic (): void {
+  Haptic.trigger('impactMedium');
+}
+
+export const useAnimatedStyleWithGesture = ({
+  disableHaptic,
+  disableLongPress,
+  onPress,
+  onLongPress,
+}: AnimatedStyleWithGestureConfig): {
+  pressGesture: TapGesture,
+  longPressGesture: LongPressGesture,
+  capStyle: ViewStyle,
+  dimStyle: ViewStyle,
+} => {
+  const capPosition = useSharedValue(0);
+  const dimPosition = useSharedValue(0);
+  const capStyle = useAnimatedStyle(() => ({ top: capPosition.value }));
+  const dimStyle = useAnimatedStyle(() => ({ top: capPosition.value, width: `${dimPosition.value}%` }));
+
+  const pressGesture = useMemo(() => (
+    Gesture.Tap()
+      .onTouchesDown(() => {
+        // Haptic feedback on only iOS
+        Platform.OS === 'ios' && !disableHaptic && runOnJS(triggerHaptic)();
+        capPosition.value = PRESS_DEPTH;
+      })
+      .onTouchesUp(() => {
+        capPosition.value = withTiming(0, { duration: RELEASE_DURATION });
+      })
+      .onEnd(() => onPress && runOnJS(onPress)())
+  ), [capPosition, disableHaptic, onPress]);
+
+  const longPressGesture = useMemo(() => {
+    const gesture = Gesture
+      .LongPress()
+      .minDuration(LONG_PRESS_DURATION);
+    
+    if (!disableLongPress) {
+      gesture.onBegin(() => {
+        dimPosition.value = withDelay(
+          LONG_PRESS_DELAY,
+          withTiming(100, {
+            duration: LONG_PRESS_DURATION - LONG_PRESS_DELAY
+          })
+        );
+      })
+      .onStart(() => onLongPress && runOnJS(onLongPress)())
+      .onEnd(() => {
+        capPosition.value = withTiming(0, { duration: RELEASE_DURATION });
+      })
+      .onFinalize(() => {
+        dimPosition.value = withTiming(0, { duration: RELEASE_DURATION });
+      });
+    }
+
+    return gesture;
+  }, [capPosition, dimPosition, disableLongPress, onLongPress]);
+
+  return { pressGesture, longPressGesture, capStyle, dimStyle };
+};
