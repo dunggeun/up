869ebd55b88114cec384b97e9cb9dbe5a78d7f42
leegeunhyeok <dev.ghlee@gmail.com>
2023-01-18 03:53:02 +0900

    chore: react-native-sqlite-storage 환경 구성
    - react-native-sqlite-storage 추가
    - SQLite 데이터베이스 추가
    - Android/iOS 빌드 환경 구성
    
    refs #79

--- react-native.config.js
+++ react-native.config.js
@@ -20,6 +20,16 @@ module.exports = {
         android: null,
       },
     },
+    'react-native-sqlite-storage': {
+      platforms: {
+        android: {
+          sourceDir:
+            '../node_modules/react-native-sqlite-storage/platforms/android-native',
+          packageImportPath: 'import org.pgsqlite.SQLitePluginPackage;',
+          packageInstance: 'new SQLitePluginPackage()'
+        },
+      },
+    },
   },
   assets:['./assets/fonts/'],
 };
