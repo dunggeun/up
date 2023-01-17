433773504b54db5b5690b8239c6f8e5abfc14963
leegeunhyeok <dev.ghlee@gmail.com>
2023-01-18 03:53:02 +0900

    chore: react-native-sqlite-storage 환경 구성
    - react-native-sqlite-storage 추가
    - SQLite 데이터베이스 추가
    - Android/iOS 빌드 환경 구성
    
    refs #79

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import 'react-native-gesture-handler';
+import SQLite from 'react-native-sqlite-storage';
 import { AppRegistry } from 'react-native';
 import { App } from '@/App';
 import { name as appName } from './app.json';
+
+SQLite.enablePromise(true);
 
 AppRegistry.registerComponent(appName, () => App);
