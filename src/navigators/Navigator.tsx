4234af52875c9e6c614615e02d1e3d6eeb59729d
leegeunhyeok <dev.ghlee@gmail.com>
2023-01-20 02:45:08 +0900

    feat: 앱 로드 로직 추가 및 suspense 적용
    네비게이터 렌더링 이전에 앱 상태를 로드하고, 이후에 네비게이터 렌더링
    하도록 구현함 (+ 로딩 중일 때, 스플래시 화면 노출)
    
    close #81

--- src/navigators/Navigator.tsx
+++ src/navigators/Navigator.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { SplashScreen } from 'src/screens';
 import { RootStackNavigator } from './RootStack';
 
 export function Navigator(): JSX.Element {
   return (
-    <>
+    <Suspense fallback={<SplashScreen />} >
       <RootStackNavigator />
       {/* other stacks */}
-    </>
+    </Suspense>
   );
 }
