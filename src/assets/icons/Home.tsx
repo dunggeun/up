6179ed73bae6712b4827233bcff4f1cd4628ff2a
leegeunhyeok <dev.ghlee@gmail.com>
2023-01-31 22:51:57 +0900

    chore: svg 아이콘 추가
    - Home, Menu, Profile
    
    refs #96

---
+++ src/assets/icons/Home.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Svg, { Path, type SvgProps } from 'react-native-svg';
+
+export function Home(props: SvgProps): JSX.Element {
+  return (
+    <Svg
+      fill="none"
+      height={24}
+      viewBox="0 0 25 24"
+      width={25}
+      {...props}
+    >
+      <Path
+        d="M5.5 20h14v-9.5l-7-6-7 6V20z"
+        stroke="currentColor"
+        strokeLinejoin="round"
+        strokeWidth={1.4}
+      />
+    </Svg>
+  );
+}
