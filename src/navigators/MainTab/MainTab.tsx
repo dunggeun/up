6aee5b5a0a6ba5d3f30b3b10223ed783dfffff5d
leegeunhyeok <dev.ghlee@gmail.com>
2023-02-01 03:08:21 +0900

    feat: TabBar 컴포넌트 구현
    커스텀 스타일 적용을 위해 TabBar 컴포넌트를 커스터마이징하여 구현함
    
    refs #101

--- src/navigators/MainTab/MainTab.tsx
+++ src/navigators/MainTab/MainTab.tsx
@@ -1,16 +1,11 @@
 import { useMemo } from 'react';
-import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { useSx, useDripsyTheme } from 'dripsy';
+import { useDripsyTheme } from 'dripsy';
 import { Home, Menu, Profile } from 'src/assets/icons';
-import { CONTAINER_MAX_WIDTH } from 'src/constants';
+import { TabBar } from 'src/components';
 
 import type { MainTabScreenParamList } from './types';
-
-const TAB_BAR_HEIGHT = 70;
-const TAB_BAR_MIN_WIDTH = 250;
-const BORDER_WIDTH = 2;
 
 const MainTab = createBottomTabNavigator<MainTabScreenParamList>();
 
@@ -19,64 +14,43 @@ function Empty(): null {
 }
 
 export function MainTabNavigator(): JSX.Element {
-  const sx = useSx();
   const { theme } = useDripsyTheme();
   const { bottom } = useSafeAreaInsets();
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const tabBarStyle = sx({
-    alignSelf: 'center',
-    width: '50%',
-    minWidth: TAB_BAR_MIN_WIDTH,
-    maxWidth: CONTAINER_MAX_WIDTH,
-    height: TAB_BAR_HEIGHT,
-    padding: '$04',
-    paddingBottom: '$04',
-    marginBottom: Platform.OS === 'web' ? '$04' : bottom,
-    borderRadius: '$full',
-    borderWidth: BORDER_WIDTH,
-    borderTopWidth: BORDER_WIDTH,
-    borderColor: '$text_primary',
-    borderTopColor: '$text_primary',
-    backgroundColor: '$white',
-  });
-
-  const tabBarItemOptions = useMemo(() => ({
+  const tabBarOptions = useMemo(() => ({
+    headerShown: false,
     tabBarShowLabel: false,
     tabBarActiveTintColor: theme.colors.$brand,
     tabBarInactiveTintColor: theme.colors.$text_tertiary,
-    tabBarItemStyle: {
-      paddingBottom: theme.space.$01,
+    tabBarStyle: {
+      marginBottom: bottom || theme.space.$04,
     },
     tabBarIconStyle: {
       transform: [{ scale: 1.5 }]
     },
-  }), [theme]);
+  }), [theme, bottom]);
 
   return (
     <MainTab.Navigator
       backBehavior="none"
       initialRouteName="Home"
-      screenOptions={{
-        headerShown: false,
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        tabBarStyle,
-      }}
+      screenOptions={tabBarOptions}
+      tabBar={TabBar}
     >
       <MainTab.Screen
         component={Empty}
         name="User"
-        options={{ ...tabBarItemOptions, tabBarIcon: Profile }}
+        options={{ tabBarIcon: Profile }}
       />
       <MainTab.Screen
         component={Empty}
         name="Home"
-        options={{ ...tabBarItemOptions, tabBarIcon: Home }}
+        options={{ tabBarIcon: Home }}
       />
       <MainTab.Screen
         component={Empty}
         name="Menu"
-        options={{ ...tabBarItemOptions, tabBarIcon: Menu }}
+        options={{ tabBarIcon: Menu }}
       />
     </MainTab.Navigator>
   );
