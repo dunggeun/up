731f5d10f648550dd3ebc461eb4975be0e7d0a08
leegeunhyeok <dev.ghlee@gmail.com>
2023-02-02 05:14:22 +0900

    feat: 네비게이션 이동 처리를 위한 유틸 구현
    navigator 객체 없이 전역 환경에서 navigate 처리 가능하도록 하는 유틸
    함수를 구현함
    
    close #103

---
+++ src/navigators/types.ts
@@ -0,0 +1,6 @@
+import type { MainTabScreenParamList } from './MainTab/types';
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+export type NavigatorScreens = {
+  Main: MainTabScreenParamList;
+}
