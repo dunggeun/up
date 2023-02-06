887b5548efe601d9775fe04982081a9f52262afd
leegeunhyeok <dev.ghlee@gmail.com>
2023-02-07 02:23:00 +0900

    feat: debounce 로직 구현
    - debounce 유틸 추가
    - useDebounce 훅 추가
    
    refs #119

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,14 @@
+import { useRef } from 'react';
+import { debounce } from 'src/utils';
+
+export function useDebounce<T>(
+  callback: (...args: T[]) => void, delay: number
+): { trigger: (...args: T[]) => void } {
+  const debounceRef = useRef<(...args: T[]) => void>();
+
+  if (!debounceRef.current) {
+    debounceRef.current = debounce(callback, delay);
+  }
+
+  return { trigger: (...args) => debounceRef.current?.(...args) };
+}
