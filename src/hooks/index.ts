da649af478aa814f00632f2fc7ae8ab6558e11c0
leegeunhyeok <dev.ghlee@gmail.com>
2023-02-07 02:23:00 +0900

    feat: debounce 로직 구현
    - debounce 유틸 추가
    - useDebounce 훅 추가
    
    refs #119

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
+export * from './useDebounce';
 export * from './useIsAuthorized';
 export * from './useMainTabBarInset';
--- src/utils/async.ts
+++ src/utils/async.ts
@@ -3,3 +3,12 @@ export const delay = (duration: number): Promise<void> => {
     setTimeout(resolve, duration);
   });
 };
+
+// eslint-disable-next-line @typescript-eslint/no-shadow
+export const debounce = <T>(callback: (...args: T[]) => void, delay: number): (...args: T[]) => void => {
+  let timer: NodeJS.Timeout;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => callback(...args), delay);
+  };
+};
