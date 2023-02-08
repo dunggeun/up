8fbc8fdbf40f9e184e01b559b48f85250c3691f1
leegeunhyeok <dev.ghlee@gmail.com>
2023-02-09 01:01:32 +0900

    feat: 스플래시 및 랜딩 화면에 로고 추가
    close #125

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,3 +5,7 @@ export const VERSION = version;
 export const APP_MINIMUM_LOADING_DURATION = 1000;
 
 export const CONTAINER_MAX_WIDTH = 600;
+export const APP_BAR_HEIGHT = 56;
+
+export const LANDING_LOGO_SIZE = 180;
+export const LANDING_LOGO_MARGIN = 100;
--- src/screens/Landing/Landing.tsx
+++ src/screens/Landing/Landing.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
-import { styled, Container } from 'dripsy';
+import { styled, Container, View, Image } from 'dripsy';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { t } from 'src/translations';
 import { presets } from 'src/themes';
 import { Button, H1, AppBar } from 'src/designs';
+import { t } from 'src/translations';
+import { LANDING_LOGO_SIZE, LANDING_LOGO_MARGIN } from 'src/constants';
+import Logo from 'src/assets/images/logo.png';
 
 import type { RootStackProps } from 'src/navigators/RootStack/types';
 
@@ -16,6 +17,18 @@ const Content = styled(View)({
 
 const PageTitleArea = styled(View)({
   paddingY: '$04',
+});
+
+const LogoArea = styled(View)({
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  paddingBottom: LANDING_LOGO_MARGIN,
+});
+
+const LogoImage = styled(Image)({
+  width: LANDING_LOGO_SIZE,
+  height: LANDING_LOGO_SIZE,
 });
 
 const ButtonArea = styled(View)({
@@ -35,6 +48,9 @@ export function Landing ({ navigation }: LandingProps): JSX.Element {
           <PageTitleArea>
             <H1 variant="primary">{t('message.greeting')}</H1>
           </PageTitleArea>
+          <LogoArea>
+            <LogoImage source={Logo} />
+          </LogoArea>
         </Content>
         <ButtonArea>
           <Button
