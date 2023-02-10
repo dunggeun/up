c774b8c710d296f5a380ce4f0a525417ba9ca1fe
leegeunhyeok <dev.ghlee@gmail.com>
2023-02-10 21:58:09 +0900

    feat: 탭 아이템 선택시의 햅틱 피드백 타입 변경
    - impactLight -> selection
    
    close #137

--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -64,7 +64,7 @@ export function TabBar ({ state, descriptors, navigation }: BottomTabBarProps): 
             : options.tabBarInactiveTintColor;
 
           const onPress = (): void => {
-            triggerHaptic();
+            triggerHaptic('selection');
 
             const event = navigation.emit({
               type: 'tabPress',
