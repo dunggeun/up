42e0f986716069fe83f92140c0c5e967cc902511
leegeunhyeok <dev.ghlee@gmail.com>
2023-02-15 23:45:29 +0900

    feat: AppBar 하단에 그림자 추가
    그라데이션 그림자를 추가함
    
    - shadow prop 추가 (기본: false)
    
    close #148

--- src/designs/molecules/AppBar/AppBar.tsx
+++ src/designs/molecules/AppBar/AppBar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { styled, View, Pressable } from 'dripsy';
+// Require cycle 이슈 방지를 위해 전체 경로로 참조
+import { LinearGradient } from 'src/components/LinearGradient';
 import { H2 } from 'src/designs/atoms';
 import { Icons } from 'src/assets';
 import { t } from 'src/translations';
@@ -7,10 +9,12 @@ import { APP_BAR_HEIGHT, HIT_SLOP } from 'src/constants';
 
 export interface AppBarProps {
   title?: string;
+  shadow?: boolean;
   onBackPress?: () => void;
   onClosePress?: () => void;
 }
 
+const SHADOW_HEIGHT = 16;
 const ACCESSIBILITY = {
   title: t('label.title'),
   back: t('label.go_back'),
@@ -19,12 +23,24 @@ const ACCESSIBILITY = {
 } as const;
 
 const Container = styled(View)({
+  position: 'relative',
   flexDirection: 'row',
   justifyContent: 'space-between',
   alignItems: 'center',
   height: APP_BAR_HEIGHT,
   paddingX: '$04',
   backgroundColor: '$white',
+});
+
+const AppBarShadow = styled(LinearGradient)({
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  right: 0,
+  height: SHADOW_HEIGHT,
+  marginBottom: -SHADOW_HEIGHT,
+  zIndex: 3,
+  elevation: 3,
 });
 
 const SideButtonArea = styled(View)({
@@ -41,11 +57,13 @@ const SideButton = styled(Pressable)({
 
 export function AppBar ({
   title = '',
+  shadow = false,
   onBackPress,
   onClosePress,
 }: AppBarProps): JSX.Element {
   return (
     <Container>
+      {shadow ? <AppBarShadow color="white" rotate={90} toOpacity={0} /> : null}
       <SideButtonArea>
         {onBackPress ? (
           <SideButton
