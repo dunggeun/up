94d22ca5397329f9006ab165ba3de75e72bda132
leegeunhyeok <dev.ghlee@gmail.com>
2023-02-17 01:10:18 +0900

    refactor: import cycle 이슈 방지를 위해 참조 구문 변경
    refs #159

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export * from './FadeInView';
 export * from './KeyboardAvoidingView';
 export * from './SafeAreaView';
 export * from './Modal';
-export * from './LinearGradient';
 export * from './ListItem';
 export * from './Section';
 export * from './TabBar';
