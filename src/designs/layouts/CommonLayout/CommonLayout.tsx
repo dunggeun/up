899f147d032a9b9b0a1871d064e756246283078a
leegeunhyeok <dev.ghlee@gmail.com>
2023-02-27 15:17:03 +0900

    feat: CommonLayout 개선
    - KeyboardAvoidingView 지원 추가
    - 스크롤 활성화/비활성화 지원 추가
    - Footer 그림자 레이아웃 변경
    
    refs #165

--- src/designs/layouts/CommonLayout/CommonLayout.tsx
+++ src/designs/layouts/CommonLayout/CommonLayout.tsx
@@ -1,13 +1,19 @@
-import React, { type ReactNode } from 'react';
+import React, { type PropsWithChildren } from 'react';
 import { Animated } from 'react-native';
 import { styled, Container as DripsyContainer, View } from 'dripsy';
+import { SafeAreaView, type SafeAreaViewProps } from 'src/components/SafeAreaView';
+import { KeyboardAvoidingView } from 'src/components/KeyboardAvoidingView';
 import { LinearGradient } from 'src/components/LinearGradient';
-import { SafeAreaView } from 'src/components/SafeAreaView';
 import { AppBar, type AppBarProps } from 'src/designs/molecules/AppBar';
 import { SHARED_CONFIG } from 'src/constants';
 
 const HeaderWrapper = styled(View)({
   marginX: '-$04',
+});
+
+const ScrollView = styled(Animated.ScrollView)({
+  marginX: '-$04',
+  paddingX: '$04',
 });
 
 const BodyWrapper = styled(View)({
@@ -21,20 +27,34 @@ const FooterWrapper = styled(View)(({ withPadding }: { withPadding: boolean }) =
   paddingBottom: withPadding ? '$04' : 0,
 }));
 
+const SHADOW_HEIGHT = 16;
 const FooterShadow = styled(LinearGradient)({
   position: 'absolute',
   left: 0,
   right: 0,
-  gap: '$04',
-  paddingX: '$04',
-  paddingBottom: '$04',
+  height: SHADOW_HEIGHT,
+  marginTop: -SHADOW_HEIGHT,
+  zIndex: 3,
+  elevation: 3,
 });
 
-export function CommonLayout ({ children }: { children?: ReactNode }): JSX.Element {
+interface CommonLayoutProps extends SafeAreaViewProps {
+  keyboardAvoiding?: boolean;
+}
+
+export function CommonLayout ({
+  children,
+  keyboardAvoiding,
+  ...props
+}: PropsWithChildren<CommonLayoutProps>): JSX.Element {
   return (
-    <SafeAreaView>
+    <SafeAreaView {...props}>
       <DripsyContainer>
-        {children}
+        {keyboardAvoiding ? (
+          <KeyboardAvoidingView>
+            {children}
+          </KeyboardAvoidingView>
+        ) : children}
       </DripsyContainer>
     </SafeAreaView>
   );
@@ -48,17 +68,28 @@ CommonLayout.Header = function Header (props: Omit<AppBarProps, 'shadow'>): JSX.
   );
 };
 
-CommonLayout.Body = function Body ({ children }: { children?: ReactNode }): JSX.Element {
+interface BodyProps {
+  scrollEnabled?: boolean;
+}
+
+CommonLayout.Body = function Body ({
+  children,
+  scrollEnabled = true
+}: PropsWithChildren<BodyProps>): JSX.Element {
+  const Container = scrollEnabled ? ScrollView : View;
+  const additionalProps = scrollEnabled ? SHARED_CONFIG.scrollableViewProps : null;
+
   return (
-    <Animated.ScrollView {...SHARED_CONFIG.scrollableViewProps}>
+    <Container {...additionalProps} style={{ flex: 1 }}>
       <BodyWrapper>
         {children}
       </BodyWrapper>
-    </Animated.ScrollView>
+    </Container>
   );
 };
 
-CommonLayout.Footer = function Footer ({ children }: { children?: ReactNode }): JSX.Element {
+// eslint-disable-next-line @typescript-eslint/ban-types
+CommonLayout.Footer = function Footer ({ children }: PropsWithChildren<{}>): JSX.Element {
   return (
     <FooterWrapper withPadding={Boolean(children)}>
       <FooterShadow color="white" fromOpacity={0} rotate={90} toOpacity={1} />
