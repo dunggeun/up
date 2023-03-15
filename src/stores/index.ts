8e774b37d015aa0ab4c6858a66abac8145870e94
leegeunhyeok <dev.ghlee@gmail.com>
2023-03-16 00:37:34 +0900

    refactor: 사용하지 않는 atom 제거

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,2 +1,1 @@
 export * from './machines';
-export * from './recoil';
--- src/stores/recoil/quest/withActiveQuest.ts
+++
@@ -1,8 +0,0 @@
-import { selector } from 'recoil';
-import { questList } from './atom';
-
-export const activeQuestList = selector({
-  key: 'activeQuestList',
-  // eslint-disable-next-line camelcase
-  get: ({ get }) => get(questList).filter(({ finished_at }) => !finished_at),
-});
