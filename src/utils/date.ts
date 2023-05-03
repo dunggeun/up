71796be3f9b041907e4e0d9b9188252677813cf8
leegeunhyeok <dev.ghlee@gmail.com>
2023-05-03 23:01:47 +0900

    fix: diffBeforeToday 계산 오류 수정
    비교 대상 날짜가 24시간 미만인 경우 잘못된 결과가 반환되던 문제 수정
    
    close #244

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,5 +1,12 @@
 import dayjs from 'dayjs';
 
+const DIFF_BEFORE_TODAY_UNIT = 'days';
+
 export const diffBeforeToday = (timestamp: number): number => {
-  return dayjs().diff(timestamp, 'days');
+  return dayjs()
+    .startOf(DIFF_BEFORE_TODAY_UNIT)
+    .diff(
+      dayjs(timestamp).startOf(DIFF_BEFORE_TODAY_UNIT),
+      DIFF_BEFORE_TODAY_UNIT,
+    );
 };
