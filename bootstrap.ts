45283e760e591c30d4c8537b9a9189ea50941a0a
leegeunhyeok <dev.ghlee@gmail.com>
2023-05-04 00:18:26 +0900

    feat: Logger 구현
    close #223

---
+++ bootstrap.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable no-console */
+import 'node-self';
+import 'react-native-gesture-handler';
+
+Object.defineProperties(self, {
+  _log: {
+    value: console.log,
+    configurable: false,
+  },
+  _warn: {
+    value: console.warn,
+    configurable: false,
+  },
+  _error: {
+    value: console.error,
+    configurable: false,
+  },
+});
---
+++ src/types/self.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable no-var */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+declare namespace globalThis {
+  var _log: (...args: any[]) => void;
+  var _warn: (...args: any[]) => void;
+  var _error: (...args: any[]) => void;
+}
