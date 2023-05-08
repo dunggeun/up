2dd2505505e1fe7e2c95e619f458f22da4829110
leegeunhyeok <dev.ghlee@gmail.com>
2023-05-08 16:56:29 +0900

    fix(web): bootstrap 코드 추가
    close #269

--- bootstrap.web.ts
+++ bootstrap.web.ts
@@ -1,9 +1,7 @@
 // eslint-disable-next-line eslint-comments/disable-enable-pair
 /* eslint-disable no-console */
-import 'node-self';
+import 'setimmediate';
 import 'react-native-reanimated';
-import 'react-native-gesture-handler';
-import 'react-native-get-random-values';
 
 Object.defineProperties(self, {
   _log: {
