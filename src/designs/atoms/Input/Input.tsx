40e1ad71e7eab718c3c81d94c3507d8aed886351
leegeunhyeok <dev.ghlee@gmail.com>
2023-05-20 16:45:23 +0900

    fix(android): 비밀번호 입력 필드 폰트 수정

--- src/designs/atoms/Input/Input.tsx
+++ src/designs/atoms/Input/Input.tsx
@@ -1,5 +1,6 @@
 import React, { forwardRef, type ComponentPropsWithRef } from 'react';
 import type { TextInput as RNTextInput } from 'react-native';
+import { Platform } from 'react-native';
 import { styled, useDripsyTheme, TextInput } from 'dripsy';
 
 type TextInputProps = ComponentPropsWithRef<typeof TextInput>;
@@ -10,15 +11,22 @@ export type InputRef = RNTextInput;
 
 const BORDER_WIDTH = 2;
 
-const StyledTextInput = styled(TextInput)({
-  paddingX: '$04',
-  paddingY: '$03',
-  borderRadius: '$input',
-  borderWidth: BORDER_WIDTH,
-  borderColor: '$border',
-  color: '$text_primary',
-  fontSize: '$h2',
-});
+const StyledTextInput = styled(TextInput)(
+  ({ isPassword }: { isPassword?: boolean }) => ({
+    paddingX: '$04',
+    paddingY: '$03',
+    borderRadius: '$input',
+    borderWidth: BORDER_WIDTH,
+    borderColor: '$border',
+    color: '$text_primary',
+    fontSize: '$h2',
+    // 안드로이드에서 비밀번호 대체 문자가 투명하게 보이는 이슈가 있어
+    // 비밀번호 입력 필드인 경우 기본 폰트를 사용하도록 함
+    ...(isPassword && Platform.OS === 'android'
+      ? { fontFamily: 'system' }
+      : null),
+  }),
+);
 
 export const Input = forwardRef<InputRef, InputProps>(function Input(
   props,
@@ -32,6 +40,7 @@ export const Input = forwardRef<InputRef, InputProps>(function Input(
     // @ts-ignore
     <StyledTextInput
       {...props}
+      isPassword={props.secureTextEntry}
       placeholderTextColor={theme.colors.$text_tertiary}
       ref={ref}
     />
