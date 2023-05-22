81044fa15bd1a1bee44f1cacc434e422e0fc9ae9
leegeunhyeok <dev.ghlee@gmail.com>
2023-05-23 04:46:19 +0900

    fix: 깃 정리 스크립트 이슈 수정

--- scripts/purge-git-release.sh
+++ scripts/purge-git-release.sh
@@ -3,9 +3,9 @@
 version=$1
 
 # purge from staging
-git tag -d $version
+git tag -d "v$version"
 git branch -D "release/$version"
 
 # purge from remote
-git push origin -d $version
+git push origin -d "v$version"
 git push origin -d "release/$version"
